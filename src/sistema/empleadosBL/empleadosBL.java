e1644473bb42e434b8ca588d46e83a15196ccc61
anyistatiana <ttatianamirandaa@gmail.com>
2022-05-17 14:27:26 -0500

    Se agrego campo telefono e imagen de perfil: Kevin Araque y Anyis Tatiana

---
+++ src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+import javax.swing.JTextField;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Kevin y Anyis
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre, correo, Telefono;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+     public String getTelefono() {
+        return Telefono;
+    }
+    
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    
+
+}
